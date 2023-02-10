84f5393ed09a84c59328918c472f1c6b35866a8e
Bushido Gino <bushidog@student.21-school.ru>
2023-02-10 15:47:31 +0000

    Add new file

---
+++ src/s21_string.h
@@ -0,0 +1,34 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define S21_NULL (void *)0
+
+void *s21_memchr(const void *str, int c, size_t n);
+int s21_memcmp(const void *str1, const void *str2, size_t n);
+void *s21_memcpy(void *dest, const void *src, size_t n);
+void *s21_memmove(void *dest, const void *src, size_t n);
+void *s21_memset(void *str, int c, size_t n);
+char *s21_strcat(char *dest, const char *src);
+char *s21_strncat(char *dest, const char *src, size_t n);
+char *s21_strchr(const char *str, int c);
+int s21_strcmp(const char *str1, const char *str2);
+int s21_strncmp(const char *str1, const char *str2, size_t n);
+char *s21_strcpy(char *dest, const char *src);
+char *s21_strncpy(char *dest, const char *src, size_t n);
+size_t s21_strcspn(const char *str1, const char *str2);
+char *s21_strerror(int errnum);
+size_t s21_strlen(const char *str);
+char *s21_strpbrk(const char *str1, const char *str2);
+char *s21_strrchr(const char *str, int c);
+size_t s21_strspn(const char *str1, const char *str2);
+char *s21_strstr(const char *haystack, const char *needle);
+char *s21_strtok(char *str, const char *delim);
+
+int s21_sprintf(char *buf, const char *format, ...);
+int s21_sscanf(char *str1, const char *format, ...);
+
+void *s21_to_upper(const char *str);
+void *s21_to_lower(const char *str);
+void *s21_insert(const char *src, const char *str, size_t start_index);
+void *s21_trim(const char *src, const char *trim_chars);
